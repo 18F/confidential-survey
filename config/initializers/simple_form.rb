1e87a6dd8215ea2c6d486b799616eb14f7d216b8
Jacob Harris <jacob.harris@gsa.gov>
2015-11-04 09:59:45 -0500

    First cut applying US Web Design Standards
    Still not entirely pretty, but this is a good start of the applying the
    US Web Design Standards to the look of the form

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -58,7 +58,7 @@ SimpleForm.setup do |config|
   # Defaults to :nested for bootstrap config.
   #   inline: input + label
   #   nested: label > input
-  config.boolean_style = :nested
+  config.boolean_style = :inline
 
   # Default class for buttons
   config.button_class = 'btn'
@@ -84,14 +84,14 @@ SimpleForm.setup do |config|
   # config.collection_value_methods = [ :id, :to_s ]
 
   # You can wrap a collection of radio/check boxes in a pre-defined tag, defaulting to none.
-  # config.collection_wrapper_tag = nil
+  config.collection_wrapper_tag = :ul
 
   # You can define the class to use on all collection wrappers. Defaulting to none.
-  # config.collection_wrapper_class = nil
+  config.collection_wrapper_class = 'usa-unstyled-list'
 
   # You can wrap each item in a collection of radio/check boxes with a tag,
   # defaulting to :span.
-  # config.item_wrapper_tag = :span
+  config.item_wrapper_tag = :li
 
   # You can define a class to use in all item wrappers. Defaulting to none.
   # config.item_wrapper_class = nil
